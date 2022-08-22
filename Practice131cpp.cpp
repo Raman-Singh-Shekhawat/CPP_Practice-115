1f4a926351e9a5cc93df31bbed3b6cc5e5149d59
Raman-Singh-Shekhawat <80272309+Raman-Singh-Shekhawat@users.noreply.github.com>
2022-08-22 23:27:00 +0530

    Add files via upload

---
+++ Practice131cpp.cpp
@@ -0,0 +1,37 @@
+// 131. Bubble Sort (Iterative) LinkedList: Given a singly linked list of integers, sort it using 'Bubble Sort.' Note: No need to print the list, it has already been taken care. Only return the new head to the list.
+
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+        };
+
+*****************************************************************/
+SwapAlternates(Node *head)
+{
+
+    Node *temp = head;
+
+    while (temp)
+    {
+    }
+}
+
+Node *bubbleSort(Node *head)
+{
+    Node *temp = head;
+
+    while (temp)
+    {
+    }
+}
